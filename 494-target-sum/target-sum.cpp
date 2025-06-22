b8a894b5b35187aea3218a493434d61cbdb25751
Purav Saluja <123455357+Purav001@users.noreply.github.com>
2025-06-22 14:56:43 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (51.32%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
-    int solve(vector<int>& nums,int n, int target){
+    vector<vector<int>> dp;
+    int solve(vector<int>& nums,int n,int target){
         if(n==0){
-            if(nums[0]==0 && target==0) return 2;
-            if(abs(target)==nums[n]) return 1;
+            if(target==0 && nums[0]==0) return 2; // +0 and -0
+            if(target==0 || target==nums[n]) return 1;
             return 0;
         }
-        return solve(nums,n-1,target-nums[n])+solve(nums,n-1,target+nums[n]);
+        if(dp[n][target]!=-1) return dp[n][target];
+        int nt=solve(nums,n-1,target);
+        int t = (target>=nums[n])? solve(nums,n-1,target-nums[n]):0;
+        return dp[n][target]=nt+t;
     }
-    int findTargetSumWays(vector<int>& nums, int target) {
+    int findTargetSumWays(vector<int>& nums, int dif) {
         int n=nums.size();
-        // dp.resize(target+1,-1);
+        int sum=0;
+        for(auto i:nums) sum+=i;
+        if(sum<dif || (sum-dif)%2) return 0;
+        int target=(sum-dif)/2;
+        dp.resize(n,vector<int>(target+1,-1));
         return solve(nums,n-1,target);
     }
 };
