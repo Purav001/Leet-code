33be056dca5d5e51e2bca034132ade5d4a7cd913
Purav Saluja <123455357+Purav001@users.noreply.github.com>
2025-06-22 15:51:30 +0530

    Time: 31 ms (27.20%) | Memory: 45.9 MB (18.78%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    // vector<vector<int>> dp;
+    // int solve(vector<int>& coins,int n,int target){
+    //     if(target==0) return 1;
+    //     if(n<0) return 0;
+    //     if(dp[n][target]!=-1) return dp[n][target];
+    //     int nt=solve(coins,n-1,target);
+    //     int t= (target>=coins[n])? solve(coins,n,target-coins[n]):0; // solve(coins,n,target-coins[n]) because we can take a coin infinite number of times;
+    //     return dp[n][target]=nt+t;
+    // }
+    int change(int amount, vector<int>& coins) {
+        int n=coins.size();
+        if(amount<0) return 0;
+        // dp.resize(n,vector<int>(amount+1,-1));
+        // return solve(coins,n-1,amount);
+        vector<vector<unsigned long long>> dp(n,vector<unsigned long long>(amount+1,0));
+        for(int i=0;i<=amount;i++) dp[0][i]= (i%coins[0]==0);
+        for(int i=1;i<n;i++){
+            for(int k=0;k<=amount;k++){
+                unsigned long long nt=dp[i-1][k];
+                unsigned long long t= 0;
+                if (k>=coins[i]) t=dp[i][k-coins[i]];
+                dp[i][k]=(nt+t);
+            }
+        }
+        return dp[n - 1][amount];
+    }
+};
