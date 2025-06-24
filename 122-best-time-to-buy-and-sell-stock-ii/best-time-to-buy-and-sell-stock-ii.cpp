9a7fd9a694a42eee7b07c21232bf35fabbdb979b
Purav Saluja <123455357+Purav001@users.noreply.github.com>
2025-06-24 17:41:30 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (46.88%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -6,18 +6,41 @@ public:
     // there are two changing params - n and buy 
     // for n there are prices.size() number of ways  but for buy there are only two ways 
     
-    vector<vector<long long>> dp;
-    int solve(vector<int>& prices,int n,int buy){
-        if(n==prices.size()) return 0;
-        if(dp[n][buy]!=-1) return dp[n][buy];
-        long long profit=0;
-        if(buy) profit= max((-prices[n]+solve(prices,n+1,0)),solve(prices,n+1,1));
-        else profit=max((prices[n]+solve(prices,n+1,1)),solve(prices,n+1,0));
-        return dp[n][buy]=profit;
-    }
+    // vector<vector<long long>> dp;
+    // int solve(vector<int>& prices,int n,int buy){
+    //     if(n==prices.size()) return 0;
+    //     if(dp[n][buy]!=-1) return dp[n][buy];
+    //     long long profit=0;
+    //     if(buy) profit= max((-prices[n]+solve(prices,n+1,0)),solve(prices,n+1,1));
+    //     else profit=max((prices[n]+solve(prices,n+1,1)),solve(prices,n+1,0));
+    //     return dp[n][buy]=profit;
+    // }
+
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        dp.resize(n,vector<long long>(2,-1));
-        return solve(prices,0,1);
+        // memoisation
+
+        // dp.resize(n,vector<long long>(2,-1));
+        // return solve(prices,0,1);
+        
+        // tabulation
+
+        // vector<vector<long long>> dp(n+1,vector<long long>(2,-1));
+        // dp[n][0]=dp[n][1]=0;
+
+        // space optimisation
+
+        vector<long long> prev(2,0),cur(2,0);
+        prev[0]=prev[1]=0;
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<=1;j++){
+                long long profit=0;
+                if(j) profit=max((-prices[i]+prev[0]),prev[1]);
+                else profit=max((prices[i]+prev[1]),prev[0]);
+                cur[j]=profit;
+            }
+            prev=cur;
+        }
+        return prev[1];
     }
 };
