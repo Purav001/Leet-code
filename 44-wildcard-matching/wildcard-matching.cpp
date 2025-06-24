2e8fa71d64a582a7539149031b2b9652361651eb
Purav Saluja <123455357+Purav001@users.noreply.github.com>
2025-06-24 16:18:27 +0530

    Time: 24 ms (83.85%) | Memory: 10 MB (94.88%) - LeetSync

---
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    // base case :
+    // if both s and p get exhausted - this means we have matched the pattern
+    // if p get exhausted - this means remaining s letter cannot be matched 
+    
+    // and iff s exhausts , we need to make sure that all the e=remaining letters of p are * - because ? and '_' corresponds to a letter that is non empty subsequence
+
+    // vector<vector<int>> dp;
+    // bool solve(string s, string p,int n,int m){
+    //     if(n<0 && m<0) return true;
+    //     if(m<0) return false;
+    //     if(n<0){
+    //         for(int i=0;i<=m;i++){
+    //             if(p[i]!='*') return false;
+    //         }
+    //         return true;
+    //     }
+    //     if(dp[n][m]!=-1) return dp[n][m];
+    //     if(p[m]=='*') return dp[n][m]=solve(s,p,n-1,m) || solve(s,p,n,m-1);
+    //     else if(p[m]=='?' || s[n]==p[m]) return dp[n][m]=solve(s,p,n-1,m-1);
+    //     return dp[n][m]=false;
+    // }
+
+
+    bool isMatch(string s, string p) {
+        int n=s.length(),m=p.length();
+        // memoisation
+
+        // dp.resize(n,vector<int>(m,-1));
+        // return solve(s,p,n-1,m-1);
+
+        // tabulation
+
+        // vector<vector<bool>> dp(n+1,vector<bool>(m+1,false));
+        // dp[0][0]=true;
+
+        // space optimisation
+        
+        vector<bool> prev(m+1,false),cur(m+1,false);
+        prev[0]=true;
+        for(int j=1;j<=m;j++){
+            if(p[j-1]=='*') prev[j]=prev[j-1]; // true only if all the prev are true
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(p[j-1]=='*') cur[j]=prev[j] || cur[j-1];
+                else if(p[j-1]=='?' || p[j-1]==s[i-1]) cur[j]=prev[j-1];
+                else cur[j]=false;
+            }
+            prev=cur;
+        }
+        return prev[m];
+    }
+};
