df2766deae7d261ff28deec2fa88c3a69bda1b73
Purav Saluja <123455357+Purav001@users.noreply.github.com>
2025-06-26 16:12:52 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (94.47%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -9,7 +9,7 @@ public:
     //     return dp[n][m+1]=len;
     // }
 
-    int lengthOfLIS(vector<int>& nums) {
+    int lengthOfLISViaDP(vector<int>& nums) {
         int n=nums.size();
         // memoisation
 
@@ -33,18 +33,52 @@ public:
         // }
         // return prev[0];
 
-        // more optimised version
+        // more optimised version + printing hte subsequence
 
-        vector<int> dp(n,1);
-        int maxi=0;
+        vector<int> dp(n,1),hash(n,0);
+        int maxi=0,last=0;
         for(int i=0;i<n;i++){
+            hash[i]=i;
             for(int j=0;j<i;j++){
-                if(nums[i]>nums[j]){
-                    dp[i]=max((1+dp[j]),dp[i]);
+                if(nums[i]>nums[j] && 1+dp[j]>dp[i]){
+                    dp[i]=1+dp[j];
+                    hash[i]=j; // to store the prev smaller element in comparison to curr element in the longest subsequence
                 }
             }
-            maxi= max(maxi,dp[i]);
+            if(dp[i]>maxi){
+                maxi=dp[i];
+                last=i; // to store the index of last element of longest subsequence
+            }
         }
+        vector<int> ans(maxi);
+        int ind=maxi-2;
+        ans[ind+1]=nums[last];
+        while(hash[last]!=last){
+            last=hash[last];
+            ans[ind--]=nums[last];
+        }
+        // ans store the longest increasing subsequence
         return maxi;
+    }
+    
+    // using Binary search , most optimised code
+    int lengthOfLIS(vector<int>& nums){
+        int n=nums.size();
+        vector<int> temp;
+        temp.push_back(nums[0]);
+        int len=1;
+        for(int i=1;i<n;i++){
+            if(nums[i]>temp.back()){
+                temp.push_back(nums[i]);
+                len++;
+            }
+            else{
+                int ind= lower_bound(temp.begin(),temp.end(),nums[i])-temp.begin();
+                // lower_bound(temp.begin(),temp.end(),nums[i]) is binary search 
+                // which Finds the first position in temp where nums[i] can be inserted without violating the sorted order
+                temp[ind]=nums[i];
+            }
+        }
+        return len;
     }
 };
