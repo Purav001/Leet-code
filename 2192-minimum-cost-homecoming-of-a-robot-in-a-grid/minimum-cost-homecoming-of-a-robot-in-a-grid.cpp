9c3f201c52cb9f699778d3e59e963566c836329e
Purav Saluja <123455357+Purav001@users.noreply.github.com>
2025-06-30 17:02:24 +0530

    Time: 0 ms (100.00%) | Memory: 153.8 MB (15.69%) - LeetSync

---
+++ 2192-minimum-cost-homecoming-of-a-robot-in-a-grid/minimum-cost-homecoming-of-a-robot-in-a-grid.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+    vector<pair<int,int>> dir={{1,0},{-1,0},{0,1},{0,-1}};
+    int solve(int sx,int sy, int hx, int hy,vector<int>& rowCosts, vector<int>& colCosts){
+        if(sx==hx && sy==hy) return 0;
+        if(dp[sx][sy]!=-1) return dp[sx][sy];
+        int mini=INT_MAX;
+
+        if(sx<hx) mini=min(mini,rowCosts[sx+1]+solve(sx+1,sy,hx,hy,rowCosts,colCosts));
+        else if(sx>hx) mini=min(mini,rowCosts[sx-1]+solve(sx-1,sy,hx,hy,rowCosts,colCosts));
+
+        if(sy<hy) mini=min(mini,colCosts[sy+1]+solve(sx,sy+1,hx,hy,rowCosts,colCosts));
+        else if(sy>hy) mini=min(mini,colCosts[sy-1]+solve(sx,sy-1,hx,hy,rowCosts,colCosts));
+
+        return dp[sx][sy]=mini;
+    }
+    int minCost(vector<int>& startPos, vector<int>& homePos, vector<int>& rowCosts, vector<int>& colCosts) {
+        int sx=startPos[0],sy=startPos[1];
+        int hx=homePos[0],hy=homePos[1];
+        int n=rowCosts.size(),m=colCosts.size();
+        // memoisation
+
+        // dp.resize(n,vector<int>(m,-1));
+        // return solve(sx,sy,hx,hy,rowCosts,colCosts);
+        
+        // tabulation
+
+        // vector<vector<int>> dp(n,vector<int>(m,0));
+            
+        // if(sx<=hx) for(int i=sx+1;i<=hx;i++) dp[i][sy]=rowCosts[i]+dp[i-1][sy];
+        // else for(int i=sx-1;i>=hx;i--) dp[i][sy]=rowCosts[i]+dp[i+1][sy];
+           
+        // if(sy<=hy) for(int j=sy+1;j<=hy;j++) dp[hx][j]=colCosts[j]+dp[hx][j-1];
+        // else for(int j=sy-1;j>=hy;j--) dp[hx][j]=colCosts[j]+dp[hx][j+1];
+            
+        // return dp[hx][hy];
+
+        // space optimised approach - greedy
+
+        int total=0;
+        // first we reach the home x from start x while we are on start y
+        if(sx<=hx) for(int i=sx+1;i<=hx;i++) total+=rowCosts[i];
+        else for(int i=sx-1;i>=hx;i--) total+=rowCosts[i];
+        // than we reach home y from start y while we have reached home x
+        if(sy<=hy) for(int j=sy+1;j<=hy;j++) total+=colCosts[j];
+        else for(int j=sy-1;j>=hy;j--) total+=colCosts[j];
+        // return the cost for reaching home x and home y
+        return total;
+    }
+};
