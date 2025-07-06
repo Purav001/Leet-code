e06ff545aea5d2d43323ae76839325a43e05cb7e
Purav Saluja <123455357+Purav001@users.noreply.github.com>
2025-07-06 16:35:47 +0530

    Time: 35 ms (96.44%) | Memory: 155 MB (89.47%) - LeetSync

--- 3927-minimum-cost-path-with-alternating-directions-ii/minimum-cost-path-with-alternating-directions-ii.cpp
+++ 3927-minimum-cost-path-with-alternating-directions-ii/minimum-cost-path-with-alternating-directions-ii.cpp
@@ -27,17 +27,37 @@ public:
 
         // tabulation
 
-        dp.resize(m+1,vector<vector<long long>>(n+1,vector<long long>(2,1e15)));
-        dp[m-1][n-1][0]=dp[m-1][n-1][1]=0;
+        // dp.resize(m+1,vector<vector<long long>>(n+1,vector<long long>(2,1e15)));
+        // dp[m-1][n-1][0]=dp[m-1][n-1][1]=0;
+        // for(int i=m-1;i>=0;i--){
+        //     for(int j=n-1;j>=0;j--){
+        //         if(i==m-1 && j==n-1) continue;
+        //         long long down= (i+1<m)? (1LL*(i+2)*(j+1))+dp[i+1][j][0]:1e15;
+        //         long long right= (j+1<n)? (1LL*(i+1)*(j+2))+dp[i][j+1][0]:1e15;
+        //         dp[i][j][1]=min(down,right);
+        //         dp[i][j][0]=waitCost[i][j]+dp[i][j][1];
+        //     }
+        // }
+        // return 1+dp[0][0][1];
+
+        // space optimisation - most optimised version
+
+        // c= for current row - i row -- o for odd and e for even
+        // p= for next row - i+1 row -- o for odd and e for even
+        vector<long long> co(n+1,1e15),ce(n+1,1e15);
+        vector<long long> po(n+1,1e15),pe(n+1,1e15);
+        co[n-1]=ce[n-1]=0;
         for(int i=m-1;i>=0;i--){
             for(int j=n-1;j>=0;j--){
                 if(i==m-1 && j==n-1) continue;
-                long long down= (i+1<m)? (1LL*(i+2)*(j+1))+dp[i+1][j][0]:1e15;
-                long long right= (j+1<n)? (1LL*(i+1)*(j+2))+dp[i][j+1][0]:1e15;
-                dp[i][j][1]=min(down,right);
-                dp[i][j][0]=waitCost[i][j]+dp[i][j][1];
+                long long down= (1LL*(i+2)*(j+1))+pe[j];
+                long long right= (1LL*(i+1)*(j+2))+ce[j+1];
+                co[j]=min(down,right);
+                ce[j]=waitCost[i][j]+co[j];
             }
+            pe=ce;
+            po=co;
         }
-        return 1+dp[0][0][1];
+        return 1+po[0];
     }
 };
