111514c0375ee07c10d00d8ca9964db381afece0
Purav Saluja <123455357+Purav001@users.noreply.github.com>
2025-07-07 15:05:30 +0530

    Time: 564 ms (19.94%) | Memory: 431.2 MB (16.26%) - LeetSync

---
+++ 3863-power-grid-maintenance/power-grid-maintenance.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    // we assign a component id to each components to which they belong
+    // and we make a map to store online connections of a components
+    // as we get quesries we check based on presence og connection in online map or not
+    vector<int> comp;
+    void dfs(int node, int id,vector<vector<int>>& adj){
+        comp[node]=id;
+        for(int i:adj[node]) if(comp[i]==0) dfs(i,id,adj);
+    }
+    vector<int> processQueries(int n, vector<vector<int>>& connections, vector<vector<int>>& queries) {
+        comp.resize(n+1,0);
+        int id=0;
+        vector<vector<int>> adj(n+1);
+        for(auto i:connections){
+            adj[i[0]].push_back(i[1]);
+            adj[i[1]].push_back(i[0]);
+        }
+        // traverse all the components
+        for(int i=1;i<=n;i++) if(comp[i]==0) dfs(i,++id,adj);
+        // store online connections of a component
+        unordered_map<int,set<int>> online;
+        for(int i=1;i<=n;i++) online[comp[i]].insert(i);
+        vector<int> ans;
+        for(auto i:queries){
+            int c=comp[i[1]];
+            if(i[0]==1){
+                if(online[c].count(i[1])) ans.push_back(i[1]);
+                else if(!online[c].empty()) ans.push_back(*online[c].begin());
+                else ans.push_back(-1);
+            }
+            else online[c].erase(i[1]);
+        }
+        return ans;
+    }
+};
