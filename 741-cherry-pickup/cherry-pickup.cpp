85fc56f16e27909374433370a4497b3aea52a10a
Purav Saluja <123455357+Purav001@users.noreply.github.com>
2025-07-16 16:29:44 +0530

    Time: 43 ms (61.77%) | Memory: 28.5 MB (44.33%) - LeetSync

--- 741-cherry-pickup/cherry-pickup.cpp
+++ 741-cherry-pickup/cherry-pickup.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     // backtracking
+    // we first travel from (0,0) to (n-1,n-1) traversing all the paths 
+    // and then go back from (n-1,n-1) to (0,0) traversing all the paths corresponding to the path we reach (n-1,n-1) from (0,0)
 
     // int cnt=0;
     // void solve(vector<vector<int>>& grid,int i,int j,int n,int cherries){
@@ -25,15 +27,32 @@ public:
     //     grid[i][j]=curr;
     // }
 
-    vector<vector<vector<vector<int>>>> dp;
-    int solve(vector<vector<int>>& grid,int i,int j,int x,int y,int n){
+    // DP
+    // we asume 2 person starting from 0,0 and going to (n-1,n-1) to obtain max cherries
+    // i.e. p1 and p2 travel b1 and b2 paths respective
+    // we can think it in this way that the a single person p1 travle to (n-1,n-1) via b1 and back to (0,0) via b2
+
+    // vector<vector<vector<vector<int>>>> dp;
+    // int solve(vector<vector<int>>& grid,int i,int j,int x,int y,int n){
+    //     if(i>=n || j>=n || x>=n || y>=n || grid[i][j]==-1 || grid[x][y]==-1) return -1e8;
+    //     if(i==n-1 && j==n-1) return grid[i][j];
+    //     if(dp[i][j][x][y]!=-1) return dp[i][j][x][y];
+    //     int curr=grid[i][j];
+    //     if(i!=x || j!=y) curr+=grid[x][y];
+    //     return dp[i][j][x][y]=curr+max({solve(grid,i+1,j,x+1,y,n),solve(grid,i+1,j,x,y+1,n),solve(grid,i,j+1,x+1,y,n),solve(grid,i,j+1,x,y+1,n)});
+    // }
+
+    vector<vector<vector<int>>> dp;
+    int solve(vector<vector<int>>& grid,int i,int j,int x,int n){
+        int y=i+j-x;
         if(i>=n || j>=n || x>=n || y>=n || grid[i][j]==-1 || grid[x][y]==-1) return -1e8;
-        if(i==n-1 && j==n-1 && x==n-1 && y==n-1) return grid[i][j];
-        if(dp[i][j][x][y]!=-1) return dp[i][j][x][y];
+        if(i==n-1 && j==n-1) return grid[i][j];
+        if(dp[i][j][x]!=-1) return dp[i][j][x];
         int curr=grid[i][j];
         if(i!=x || j!=y) curr+=grid[x][y];
-        return dp[i][j][x][y]=curr+max({solve(grid,i+1,j,x+1,y,n),solve(grid,i+1,j,x,y+1,n),solve(grid,i,j+1,x+1,y,n),solve(grid,i,j+1,x,y+1,n)});
+        return dp[i][j][x]=curr+max({solve(grid,i+1,j,x,n),solve(grid,i+1,j,x+1,n),solve(grid,i,j+1,x,n),solve(grid,i,j+1,x+1,n)});
     }
+
     int cherryPickup(vector<vector<int>>& grid) {
         int n=grid.size();
         if(grid[0][0]==-1 || grid[n-1][n-1]==-1) return 0;
@@ -43,7 +62,19 @@ public:
         // return cnt;
 
         // memoisation
-        dp.resize(n,vector<vector<vector<int>>>(n,vector<vector<int>>(n,vector<int>(n,-1))));
-        return max(0,solve(grid,0,0,0,0,n));
+
+        // dp.resize(n,vector<vector<vector<int>>>(n,vector<vector<int>>(n,vector<int>(n,-1))));
+        // return max(0,solve(grid,0,0,0,0,n));
+
+        // memoisation - space optimisation
+
+        // we know that both person move and from i,j to r1,c1 and r2,c2
+        // => r1+c1=r2+c2
+        // => c2=r1+c1-r2;
+        // hence we can optimise it to 3d DP
+
+        dp.resize(n,vector<vector<int>>(n,vector<int>(n,-1)));
+        return max(0,solve(grid,0,0,0,n));
+
     }
 };
