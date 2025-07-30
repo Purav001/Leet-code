93909779b397845b41e398c56f17047c38e79be2
Purav Saluja <123455357+Purav001@users.noreply.github.com>
2025-07-30 23:40:42 +0530

    Time: 21 ms (80.70%) | Memory: 79.8 MB (88.77%) - LeetSync

--- 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.cpp
+++ 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.cpp
@@ -2,20 +2,17 @@ class Solution {
 public:
     int largestSubmatrix(vector<vector<int>>& matrix) {
         int n=matrix.size(),m=matrix[0].size();
-        vector<int> hist(m,0);
         int ans=0;
-        // consisder each roaw as histogram and its height as pillars of histogram
-        for(int i=0;i<n;i++){
-            // calculate height of histogram
+         // consisder each roaw as histogram and its height as pillars of histogram
+        for(int i=1;i<n;i++){
             for(int j=0;j<m;j++){
-                if(matrix[i][j]==0) hist[j]=0;
-                else hist[j]+=1;
+                if(matrix[i][j]==1) matrix[i][j]+=matrix[i-1][j];  // calculate height of histogram
             }
-            // sort these pillars
-            vector<int> row=hist;
-            sort(row.begin(),row.end());
-            // find the max area
-            for(int j=0;j<m;j++) ans=max(ans,row[j]*(m-j));
+        }
+        for(int i=0;i<n;i++){
+            // sort these pillars to get iterate from the shortest pillar and get the max area
+            sort(matrix[i].begin(),matrix[i].end());
+            for(int j=0;j<m;j++) ans=max(ans,matrix[i][j]*(m-j));
         }
         return ans;
     }
