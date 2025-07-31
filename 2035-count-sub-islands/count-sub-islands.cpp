a597e2604905126235d265c46085542ec65806f5
Purav Saluja <123455357+Purav001@users.noreply.github.com>
2025-07-31 23:41:28 +0530

    Time: 31 ms (75.62%) | Memory: 91.9 MB (98.08%) - LeetSync

--- 2035-count-sub-islands/count-sub-islands.cpp
+++ 2035-count-sub-islands/count-sub-islands.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     vector<pair<int,int>> dir={{1,0},{-1,0},{0,1},{0,-1}};
-    void dfs(vector<vector<int>>& grid1, vector<vector<int>>& grid2,int i,int j){
+    void dfs(vector<vector<int>>& grid2,int i,int j){
         grid2[i][j]=0;
         for(auto [dx,dy]:dir){
             int nx=i+dx,ny=j+dy;
-            if(nx>=0 && ny>=0 && nx<grid2.size() && ny<grid2[0].size() && grid2[nx][ny]==1 ) dfs(grid1,grid2,nx,ny);
+            if(nx>=0 && ny>=0 && nx<grid2.size() && ny<grid2[0].size() && grid2[nx][ny]==1 ) dfs(grid2,nx,ny);
         }
     }
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
@@ -13,14 +13,14 @@ public:
         int cnt=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid2[i][j]==1 && grid1[i][j]==0) dfs(grid1,grid2,i,j);
+                if(grid2[i][j]==1 && grid1[i][j]==0) dfs(grid2,i,j);
             }
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid2[i][j]==1){
                     cnt++;
-                    dfs(grid1,grid2,i,j);
+                    dfs(grid2,i,j);
                 }
             }
         }
