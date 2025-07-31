b77aecfd074e24ef52397104e09099e8182c77de
Purav Saluja <123455357+Purav001@users.noreply.github.com>
2025-07-31 11:23:10 +0530

    Time: 51 ms (66.80%) | Memory: 18.8 MB (81.66%) - LeetSync

---
+++ 2206-detonate-the-maximum-bombs/detonate-the-maximum-bombs.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    // traverse using dfs
+    void dfs(vector<vector<int>>& adj,int node,vector<bool>& visited,int& cnt){
+        visited[node]=true;
+        cnt++;
+        for(int i:adj[node]){
+            if(!visited[i]) dfs(adj,i,visited,cnt);
+        }
+    }
+    int maximumDetonation(vector<vector<int>>& bombs) {
+        int n=bombs.size();
+        // treat bombs as nodes and store the bombs in adjacency list 
+        // store in a way that adj[u] contains the bombs that will. be exploaded if bomb u gets exploaded
+        vector<vector<int>> adj(n);
+        for(int i=0;i<n;i++){
+            long long x1=bombs[i][0],y1=bombs[i][1],r1=(long long)bombs[i][2];
+            for(int j=0;j<n;j++){
+                if(i==j) continue;
+                long long x2=bombs[j][0],y2=bombs[j][1];
+                long long dist=(x2-x1)*(x2-x1)+(y2-y1)*(y2-y1);
+                if(dist<=r1*r1) adj[i].push_back(j);
+            }
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            vector<bool> visited(n,false);
+            int cnt=0;
+            dfs(adj,i,visited,cnt);
+            ans=max(ans,cnt);
+        }
+        return ans;
+    }
+};
