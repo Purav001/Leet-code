3e5c0e3d543c17b7f65421290dbdb3a0129f1129
Purav Saluja <123455357+Purav001@users.noreply.github.com>
2025-08-04 12:48:17 +0530

    Time: 0 ms (100.00%) | Memory: 153.8 MB (16.67%) - LeetSync

--- 3954-maximum-balanced-shipments/maximum-balanced-shipments.cpp
+++ 3954-maximum-balanced-shipments/maximum-balanced-shipments.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
+    // nums = [4, 3, 5, 1, 2]
+
+    //     \U0001f9ed Iteration 1:
+    // 	•	i = 1, nums[i] = 3, mw = 4
+    // 	•	Check: mw > nums[i] → 4 > 3 → ✅
+    // 	•	cnt++ → cnt = 1
+    // 	•	i++ → i = 2
+    // 	•	mw = nums[2] = 5
+
+    // \U0001f9ed Iteration 2:
+    // 	•	i = 3, nums[i] = 1, mw = 5
+    // 	•	Check: 5 > 1 → ✅
+    // 	•	cnt++ → cnt = 2
+    // 	•	i++ → i = 4
+    // 	•	mw = nums[4] = 2
     int maxBalancedShipments(vector<int>& nums) {
         int n=nums.size();
         int cnt=0;
-        int i=0;
-        while(i<n){
-            int mw=nums[i];
-            int j=i+1;
-            while(j<n){
-                mw=max(mw,nums[j]);
-                if(nums[j]<mw){
-                    i=j+1;
-                    cnt++;
-                    break;
-                }
-                j++;
+        for(int i=1,mw=nums[0];i<n;i++){
+            if(mw>nums[i]){
+                cnt++;
+                ++i;
             }
-            if(j==n) break;
+            mw=nums[i];
         }
         return cnt;
     }
