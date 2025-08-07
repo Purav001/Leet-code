bd99b0599f152f443e3fae13b11dd6edc4622341
Purav Saluja <123455357+Purav001@users.noreply.github.com>
2025-08-07 15:19:25 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (38.71%) - LeetSync

--- 797-rabbits-in-forest/rabbits-in-forest.cpp
+++ 797-rabbits-in-forest/rabbits-in-forest.cpp
@@ -1,19 +1,28 @@
 class Solution {
 public:
+    // we count the number of rabbits saying a number x
+    // if the number of rabbits saying x are greater than x+1 => there are rabbits of different colour that have same x+1 rabbits of same colour
+    // for eg let us say rabits saying 1 are 3 => there are two rabits of c colour and 2 more rabbits of y colour
+    // that is why we do "ceil((double)i.second/(i.first+1))*(i.first+1)"
     int numRabbits(vector<int>& answers) {
         int n=answers.size();
         unordered_map<int,int> mp;
         for(int i:answers) mp[i]++;
-        for(auto& i:mp){
-            if(i.second>(i.first+1)){
-                int j=i.second%(i.first+1);
-                i.second/=(i.first+1);
-                if(j!=0) i.second++;
-            }
-            else i.second=1;
-        }
+        // for(auto& i:mp){
+        //     if(i.second>(i.first+1)){
+        //         int j=i.second%(i.first+1);
+        //         i.second/=(i.first+1);
+        //         if(j!=0) i.second++;
+        //     }
+        //     else i.second=1;
+        // }
+        // int ans=0;
+        // for(auto i:mp) ans+=(i.first+1)*i.second;
+
+        // or
+
         int ans=0;
-        for(auto i:mp) ans+=(i.first+1)*i.second;
+        for(auto i:mp) ans+= ceil((double)i.second/(i.first+1))*(i.first+1);
         return ans;
     }
 };
