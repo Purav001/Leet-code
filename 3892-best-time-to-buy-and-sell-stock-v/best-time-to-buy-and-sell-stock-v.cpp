4f8e98a296b1c8251806200d7d7ec2c61186e627
Purav Saluja <123455357+Purav001@users.noreply.github.com>
2025-08-10 09:58:19 +0530

    Time: 171 ms (81.18%) | Memory: 142.8 MB (51.77%) - LeetSync

--- 3892-best-time-to-buy-and-sell-stock-v/best-time-to-buy-and-sell-stock-v.cpp
+++ 3892-best-time-to-buy-and-sell-stock-v/best-time-to-buy-and-sell-stock-v.cpp
@@ -1,38 +1,71 @@
 class Solution {
 public:
-    vector<vector<vector<long long>>> dp;
-    long long solve(vector<int>& prices,int n,int holding,int k){
-        if(n==prices.size() || k==0){
-            if(holding==0) return 0;
-            else return LLONG_MIN/4;
-        }
-        if(dp[n][holding][k]!=-1) return dp[n][holding][k];
-        long long profit=0;
-        if(holding==0){
-            profit=max({
-                -prices[n]+solve(prices,n+1,1,k),
-                prices[n]+solve(prices,n+1,2,k),
-                solve(prices,n+1,0,k)
-            });
-        }
-        else if(holding==1){
-            profit=max({
-                prices[n]+solve(prices,n+1,0,k-1),
-                solve(prices,n+1,1,k)
-            });
-        }
-        else{
-            profit=max({
-                -prices[n]+solve(prices,n+1,0,k-1),
-                solve(prices,n+1,2,k)
-            });
-        }
-        return dp[n][holding][k]=profit;
+    // memoisation
+
+    // holding states
+    // 0- we can either buy, short sell, os skip to next day
+    // 1- we have bought the stock , now we need to sell to complete the transaction
+    // 2- we have short sell the stock , mow we need to buy the stock
+
+    // vector<vector<vector<long long>>> dp;
+    // long long solve(vector<int>& prices,int n,int holding,int k){
+    //     if(n==prices.size() || k==0){
+    //         if(holding==0) return 0;
+    //         else return LLONG_MIN/4;
+    //     }
+    //     if(dp[n][holding][k]!=-1) return dp[n][holding][k];
+    //     long long profit=0;
+    //     if(holding==0){
+    //         profit=max({
+    //             -prices[n]+solve(prices,n+1,1,k),
+    //             prices[n]+solve(prices,n+1,2,k),
+    //             solve(prices,n+1,0,k)
+    //         });
+    //     }
+    //     else if(holding==1){
+    //         profit=max({
+    //             prices[n]+solve(prices,n+1,0,k-1),
+    //             solve(prices,n+1,1,k)
+    //         });
+    //     }
+    //     else{
+    //         profit=max({
+    //             -prices[n]+solve(prices,n+1,0,k-1),
+    //             solve(prices,n+1,2,k)
+    //         });
+    //     }
+    //     return dp[n][holding][k]=profit;
         
-    }
+    // }
     long long maximumProfit(vector<int>& prices, int k) {
         int n=prices.size();
-        dp.resize(n,vector<vector<long long>>(3,vector<long long>(k+1,-1)));
-        return solve(prices,0,0,k);
+        // memoisation
+
+        // dp.resize(n,vector<vector<long long>>(3,vector<long long>(k+1,-1)));
+        // return solve(prices,0,0,k);
+
+        // tabulation
+
+        vector<vector<vector<long long>>> dp(n+1,vector<vector<long long>>(3,vector<long long>(k+1,0)));
+        for(int i=0;i<=k;i++){
+            dp[n][0][i]=0;
+            dp[n][1][i]=-1e14;
+            dp[n][2][i]=-1e14;
+        }
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<=2;j++){
+                for(int l=0;l<=k;l++){
+                    long long nt=dp[i+1][j][l]; // nt - not take
+                    long long t=-1e14; // t- take
+                    if(l>0){
+                        if(j==0) t=max(-prices[i]+dp[i+1][1][l],prices[i]+dp[i+1][2][l]); // either long buy or short sell
+                        else if(j==1) t=prices[i]+dp[i+1][0][l-1]; // long buy
+                        else t=-prices[i]+dp[i+1][0][l-1]; // short sell
+                    }
+                    dp[i][j][l]=max(t,nt);
+                }
+            }
+        }
+        return dp[0][0][k];
     }
 };
