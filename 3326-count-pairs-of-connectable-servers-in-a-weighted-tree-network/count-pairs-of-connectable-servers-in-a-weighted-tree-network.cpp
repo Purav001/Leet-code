e678b031679a9cb39c0412530b62c59ab867556b
Purav Saluja <123455357+Purav001@users.noreply.github.com>
2025-08-11 14:01:25 +0530

    Time: 104 ms (56.47%) | Memory: 37.5 MB (65.52%) - LeetSync

---
+++ 3326-count-pairs-of-connectable-servers-in-a-weighted-tree-network/count-pairs-of-connectable-servers-in-a-weighted-tree-network.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int dfs(vector<vector<pair<int,int>>>& adj,int s,int parent,int node,int w){
+        int cnt=(w%s)?0:1;
+        for(auto [v,d]:adj[node]){
+            if(v!=parent) cnt+=dfs(adj,s,node,v,d+w);
+        }
+        return cnt;
+    }
+    vector<int> countPairsOfConnectableServers(vector<vector<int>>& edges, int signalSpeed) {
+        int n=edges.size()+1;
+        vector<vector<pair<int,int>>> adj(n);
+        for(auto edge:edges){
+            int u=edge[0],v=edge[1],w=edge[2];
+            adj[u].push_back({v,w});
+            adj[v].push_back({u,w});
+        }
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            int nodeCount=0,res=0;
+            for(auto [v,w]:adj[i]){ // iterate for each branch 
+                int cnt=dfs(adj,signalSpeed,i,v,w);
+                res+=nodeCount*cnt; // counts the number of pairs
+                nodeCount+=cnt; // accumulates the previous nodes
+            }
+            ans.push_back(res);
+        }
+        return ans;
+    }
+};
