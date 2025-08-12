6e1c033362480f027f48af794657dd5be81758a8
Purav Saluja <123455357+Purav001@users.noreply.github.com>
2025-08-12 08:53:12 +0530

    Time: 121 ms (53.90%) | Memory: 165 MB (33.60%) - LeetSync

---
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    // classic BFS+ topo sort 
+    // basically we are making a graph between ingredients and recipee
+    // we maintain a degree vector that tell us the number of ingredients needed to make a recipee
+    // we push all our supplies to queue
+    // as we iterate over our ingredients , we subtract 1 from degree of recipe in the adj list of ingredient
+    // and when the degree becomes 0 the recipee is made and we push this recipe in queue to make another recipe
+    vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
+        int n=recipes.size();
+        unordered_map<string,vector<string>> adj;
+        unordered_map<string,int> deg; // counts number of ingredient needed to make a recipe
+        for(int i=0;i<n;i++){
+            string recipe=recipes[i];
+            deg[recipes[i]]=ingredients[i].size();
+            for(auto ing:ingredients[i]) adj[ing].push_back(recipe);
+        }
+        queue<string> q;
+        for(auto i:supplies) q.push(i); // push all our supplies
+        vector<string> ans;
+        while(!q.empty()){
+            string item=q.front();
+            q.pop();
+            if(adj.count(item)){
+                for(auto recipe:adj[item]){
+                    deg[recipe]--;
+                    if(deg[recipe]==0){ // recipe is made
+                        ans.push_back(recipe);
+                        q.push(recipe); // recipe acts as an ingredient for another recipe
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
