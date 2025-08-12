8f21765aa1548ef27495eededb13d9bd5cf0b2a6
Purav Saluja <123455357+Purav001@users.noreply.github.com>
2025-08-12 12:47:43 +0530

    Time: 113 ms (65.58%) | Memory: 83.6 MB (41.61%) - LeetSync

--- 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -7,35 +7,42 @@ public:
     const int MOD=1e9+7;
     // memoisation
 
-    vector<vector<int>> dp;
-    int solve(int i,int n,int x){
-        if(n==0) return 1;
-        if(n<0 || n<pow(i,x)) return 0;
-        if(dp[n][i]!=-1) return dp[n][i];
-        int t=solve(i+1,n-pow(i,x),x); // t- take
-        int nt=solve(i+1,n,x); // nt- not take
-        return dp[n][i]=(t+nt)%MOD;
-    }
+    // vector<vector<int>> dp;
+    // int solve(int i,int n,int x){
+    //     if(n==0) return 1;
+    //     if(n<0 || n<pow(i,x)) return 0;
+    //     if(dp[n][i]!=-1) return dp[n][i];
+    //     int t=solve(i+1,n-pow(i,x),x); // t- take
+    //     int nt=solve(i+1,n,x); // nt- not take
+    //     return dp[n][i]=(t+nt)%MOD;
+    // }
 
     int numberOfWays(int n, int x) {
         // max number can be the number itself if x=1 , so we take (n+1)*(n+1) matrix;
         // memoisation
 
-        dp.resize(n+1,vector<int>(n+1,-1));
-        return solve(1,n,x);
+        // dp.resize(n+1,vector<int>(n+1,-1));
+        // return solve(1,n,x);
 
         // tabulation
-
-        // vector<vector<int>> dp(n+1,vector<int>(n+2,0));
-        // for(int i=0;i<=n;i++) dp[0][i]=1;
-        // for(int i=1;i<=n;i++){
-        //     for(int j=n;j>=1;j--){
-        //         int nt= dp[i][j+1];
-        //         int t=0;
-        //         if(i>=pow(j,x)) t=dp[i-pow(j,x)][j+1];
-        //         dp[i][j]=(t+nt)%MOD;
-        //     }
-        // }
-        // return dp[n][1];
+        vector<int> pow;
+        for(int j=1; ;j++){
+            long long val=1;
+            for(int k=0;k<x;k++) val*=j;
+            if(val>n) break;
+            pow.push_back((int)val);
+        }
+        int m=pow.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+2,0));
+        for(int i=0;i<=m+1;i++) dp[0][i]=1;
+        for(int i=1;i<=n;i++){
+            for(int j=m;j>=1;j--){
+                int nt= dp[i][j+1];
+                int t=0;
+                if(i>=pow[j-1]) t=dp[i-pow[j-1]][j+1];
+                dp[i][j]=(t+nt)%MOD;
+            }
+        }
+        return dp[n][1];
     }
 };
