34f66599d74f5c482d538da438d1bb60984df6f2
Purav Saluja <123455357+Purav001@users.noreply.github.com>
2025-08-13 08:31:02 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (14.61%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n==0) return false;
-        while(n>1){
-            if(n%3!=0) return false;
-            n/=3;
-        }
-        return n==1;
+        // loop
+
+        // if(n<=0) return false;
+        // while(n>1){
+        //     if(n%3!=0) return false;
+        //     n/=3;
+        // }
+        // return n==1;
+
+        // log
+
+        if(n<=0) return false;
+        double log= log10(n)/log10(3); // base changing formula
+        return log==floor(log); // check wheather the log value is integer or not 
     }
 };
