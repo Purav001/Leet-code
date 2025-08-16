75474fdfe3fddfef8335d0f108b32ec20f1f8fdf
Purav Saluja <123455357+Purav001@users.noreply.github.com>
2025-08-16 17:03:56 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (12.18%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.cpp
+++ 1448-maximum-69-number/maximum-69-number.cpp
@@ -3,22 +3,31 @@ public:
     int maximum69Number (int num) {
         // naive approach
 
-        int n=num,temp=0;
-        while(n>0){
-            int t=n%10;
-            temp=temp*10+t;
-            n/=10;
+        // int n=num,temp=0;
+        // while(n>0){
+        //     int t=n%10;
+        //     temp=temp*10+t;
+        //     n/=10;
+        // }
+        // int ans=0,st=1;
+        // while(temp>0){
+        //     int t=temp%10;
+        //     if(t==6 && st){
+        //         t=9;
+        //         st=0;
+        //     }
+        //     ans=ans*10+t;
+        //     temp/=10;
+        // }
+        // return ans;
+
+        // better thinking
+
+        int a=-1; // store the place where first 6 occurs from left
+        for(int n=num,d=0;n>0;n/=10,d++){
+            int r=n%10;
+            if(r==6) a=d;
         }
-        int ans=0,st=1;
-        while(temp>0){
-            int t=temp%10;
-            if(t==6 && st){
-                t=9;
-                st=0;
-            }
-            ans=ans*10+t;
-            temp/=10;
-        }
-        return ans;
+        return (a==-1)?num:int(num+3*pow(10,a));
     }
 };
