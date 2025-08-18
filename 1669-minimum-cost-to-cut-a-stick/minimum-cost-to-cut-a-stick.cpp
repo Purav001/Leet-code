146857f50dbc2c004e29515e68c8effc4cc11c75
Purav Saluja <123455357+Purav001@users.noreply.github.com>
2025-08-18 19:49:38 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (7.08%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -7,17 +7,18 @@ public:
     // to keep track of length we add 0 to left of cuts array and n to right of cuts array 
     // so that every time we cut the stick we easily get the length by cuts[j+1]-cuts[i-1]
     
-    // vector<vector<long long>> dp;
-    // long long solve(vector<int>& cuts, int i, int j){
-    //     if(i>j) return 0;
-    //     long long mini=INT_MAX;
-    //     if(dp[i][j]!=-1) return dp[i][j];
-    //     for(long long k=i;k<=j;k++){
-    //         long long cut=(cuts[j+1]-cuts[i-1])+solve(cuts,i,k-1)+solve(cuts,k+1,j);
-    //         if(cut<mini) mini=cut;
-    //     }
-    //     return dp[i][j]=mini;
-    // }
+    vector<vector<long long>> dp;
+    long long solve(vector<int>& cuts,int i,int j){
+        if(i>j) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        long long mini=LLONG_MAX;
+        for(int k=i;k<=j;k++){
+            long long cut= (cuts[j+1]-cuts[i-1])+solve(cuts,i,k-1)+solve(cuts,k+1,j);
+            if(cut<mini) mini=cut;
+        }
+        return dp[i][j]=mini;
+    }
+
     int minCost(int n, vector<int>& cuts) {
         int m=cuts.size();
         cuts.push_back(n);
@@ -26,24 +27,24 @@ public:
 
         // memoisation
 
-        // dp.resize(m+1,vector<long long>(m+1,-1));
-        // return (int)solve(cuts,1,m);
+        dp.resize(m+1,vector<long long>(m+1,-1));
+        return (int)solve(cuts,1,m);
 
         // tabulation
 
-        vector<vector<long long>> dp(m+2,vector<long long>(m+2,0));
-        for(int i=m;i>=1;i--){
-            for(int j=1;j<=m;j++){
-                if(i>j) continue;
-                long long mini= LLONG_MAX;
-                for(int k=i;k<=j;k++){
-                    long long cut=(cuts[j+1]-cuts[i-1])+dp[i][k-1]+dp[k+1][j];
-                    if(cut<mini) mini=cut;
-                }
-                dp[i][j]=mini;
-            }
-        }
-        return dp[1][m];
+        // vector<vector<long long>> dp(m+2,vector<long long>(m+2,0));
+        // for(int i=m;i>=1;i--){
+        //     for(int j=1;j<=m;j++){
+        //         if(i>j) continue;
+        //         long long mini= LLONG_MAX;
+        //         for(int k=i;k<=j;k++){
+        //             long long cut=(cuts[j+1]-cuts[i-1])+dp[i][k-1]+dp[k+1][j];
+        //             if(cut<mini) mini=cut;
+        //         }
+        //         dp[i][j]=mini;
+        //     }
+        // }
+        // return dp[1][m];
     }
 };
 auto init = atexit([]()
