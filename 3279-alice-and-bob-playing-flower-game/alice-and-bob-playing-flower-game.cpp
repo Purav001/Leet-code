4d3d89df7e726054e071b44af4f552cb14257f28
Purav Saluja <123455357+Purav001@users.noreply.github.com>
2025-08-29 14:16:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (12.44%) - LeetSync

--- 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.cpp
+++ 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     long long flowerGame(int n, int m) {
-        // long long cnt=0;
-        // for(int i=1;i<=n;i++){
-        //     for(int j=1;j<=m;j++){
-        //         if((i+j)%2!=0) cnt++;
-        //     }
-        // }
         return (long long)(1ll*n*m)/2;
     }
 };
