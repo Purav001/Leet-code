a455baa30d26312f6a194c9146babdec44828ccb
Purav Saluja <123455357+Purav001@users.noreply.github.com>
2025-09-04 15:41:31 +0530

    Time: 3 ms (53.80%) | Memory: 9.8 MB (42.86%) - LeetSync

---
+++ 2883-partition-string-into-minimum-beautiful-substrings/partition-string-into-minimum-beautiful-substrings.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    // somewhat like partition DP on strings
+    // basically we need to place cuts , such that the substring is beautiful
+
+    // dp[i]- minimum number of substrings toll index i that are beautiful 
+
+    vector<int> dp;
+    int solve(string s,int i){
+        if(i==s.length()) return 0;
+        if(dp[i]!=-1) return dp[i];
+        int mini=INT_MAX;
+        string temp="";
+        for(int j=i;j<s.length();j++){
+            temp+=s[j];
+            if(isBeautiful(temp)){
+                int cuts=solve(s,j+1);
+                if(cuts!=INT_MAX) mini=min(mini,1+cuts);
+            }
+        }
+        return dp[i]=mini;
+    }
+
+    bool isBeautiful(string s){
+        if(s[0]=='0') return false;
+        long long num=0;
+        for(char c:s){
+            num=num*2+(c-'0');
+        }
+        if(num<=0) return false;
+        while(num%5==0) num/=5;
+        return num==1;
+    }
+
+    int minimumBeautifulSubstrings(string s) {
+        int n=s.length();
+
+        dp.resize(n,-1);
+        int ans=solve(s,0);
+        return ans==INT_MAX?-1:ans;
+    }
+};
