0a8d350ab7b981e98811c5c3459cf888d66f3a8b
Purav Saluja <123455357+Purav001@users.noreply.github.com>
2025-09-04 13:24:27 +0530

    Time: 3 ms (85.82%) | Memory: 12.5 MB (62.18%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -3,41 +3,41 @@ public:
     // basically we have to partition the array in such a way that the subarray length is <=k and each element of the subarray is changed to the max element in the subarray 
     // the main objective is to get the max sum of the array
 
-    vector<int> dp;
-    int solve(vector<int>& nums,int i, int k){
-        if(i==nums.size()) return 0;
-        if(dp[i]!=-1) return dp[i];
+    // vector<int> dp;
+    // int solve(vector<int>& nums,int i, int k){
+    //     if(i==nums.size()) return 0;
+    //     if(dp[i]!=-1) return dp[i];
 
-        int len=0,maxel=INT_MIN,maxSum=INT_MIN;
-        for(int j=i;j<min((int)nums.size(),i+k);j++){
-            len++;
-            maxel=max(maxel,nums[j]);
-            int sum=len*maxel+solve(nums,j+1,k);
-            maxSum=max(maxSum,sum);
-        }
-        return dp[i]=maxSum;
-    }
+    //     int len=0,maxel=INT_MIN,maxSum=INT_MIN;
+    //     for(int j=i;j<min((int)nums.size(),i+k);j++){
+    //         len++;
+    //         maxel=max(maxel,nums[j]);
+    //         int sum=len*maxel+solve(nums,j+1,k);
+    //         maxSum=max(maxSum,sum);
+    //     }
+    //     return dp[i]=maxSum;
+    // }
 
     int maxSumAfterPartitioning(vector<int>& nums, int k) {
         int n=nums.size();
         // memoisation
 
-        dp.resize(n,-1);
-        return solve(nums,0,k);
+        // dp.resize(n,-1);
+        // return solve(nums,0,k);
 
         // tabulation
 
-        // vector<int> dp(n+1,0);
-        // for(int i=n-1;i>=0;i--){
-        //     int len=0,maxel=INT_MIN,maxsum=INT_MIN;
-        //     for(int j=i;j<min(n,i+k);j++){
-        //         len++;
-        //         maxel=max(maxel,nums[j]);
-        //         int sum= len*maxel+ dp[j+1];
-        //         maxsum=max(maxsum,sum);
-        //     }
-        //     dp[i]=maxsum;
-        // }
-        // return dp[0];
+        vector<int> dp(n+1,0);
+        for(int i=n-1;i>=0;i--){
+            int len=0,maxel=INT_MIN,maxSum=INT_MIN;
+            for(int j=i;j<min((int)nums.size(),i+k);j++){
+                len++;
+                maxel=max(maxel,nums[j]);
+                int sum=len*maxel+dp[j+1];
+                maxSum=max(maxSum,sum);
+            }
+            dp[i]=maxSum;
+        }
+        return dp[0];
     }
 };
