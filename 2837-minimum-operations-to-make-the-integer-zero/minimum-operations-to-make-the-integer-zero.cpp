0cc4df3ed89a6faeefb3a3f479825a5770b1c64e
Purav Saluja <123455357+Purav001@users.noreply.github.com>
2025-09-05 13:23:20 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (85.95%) - LeetSync

---
+++ 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    // we have to make num1 to 0 
+    // let us assume that num1 becomes 0  after k operations
+    // after k opperations 
+
+    // num1=( 2^i1 + 2^i2 ..... + 2^ik) + k * num2
+    // => num1 - k * num2 = s = ( 2^i1 + 2^i2 ..... + 2^ik)
+
+    // min values of s can be k - when all i are 0
+    // and max value can be k * 2^60
+
+    // and k lies in [1,60]
+
+    // __builtin_popcountll(s) -> number of set bits in binary representation of s -> which tell us the powers of 2 used to make s
+    // __builtin_popcountll(S) <=k && k<=s
+
+    int makeTheIntegerZero(int num1, int num2) {
+        for(int k=1;k<=60;k++){
+            long long s=num1-1ll*k*num2;
+            if(s<0) continue;
+            if(__builtin_popcountll(s)<=k && k<=s) return k;
+        }
+        return -1;
+    }
+};
