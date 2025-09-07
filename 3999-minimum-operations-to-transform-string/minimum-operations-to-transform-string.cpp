5806aab31edb9dfb30f5f0b406c053452a784e69
Purav Saluja <123455357+Purav001@users.noreply.github.com>
2025-09-07 11:43:54 +0530

    Time: 14 ms (68.42%) | Memory: 59.9 MB (5.26%) - LeetSync

--- 3999-minimum-operations-to-transform-string/minimum-operations-to-transform-string.cpp
+++ 3999-minimum-operations-to-transform-string/minimum-operations-to-transform-string.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
     int minOperations(string s) {
-        vector<int> freq(27,0);
-        for(char c:s) freq[c-'a']++;
-        int cnt=0;
-        for(int i=1;i<26;i++){
-            if(freq[i]==0) continue;
-            else{
-                freq[i+1]+=freq[i];
-                freq[i]=0;
-                cnt++;
-            }
-        }
-        return cnt;
+        // naive approach 
+
+        // vector<int> freq(27,0);
+        // for(char c:s) freq[c-'a']++;
+        // int cnt=0;
+        // for(int i=1;i<26;i++){
+        //     if(freq[i]==0) continue;
+        //     else{
+        //         freq[i+1]+=freq[i];
+        //         freq[i]=0;
+        //         cnt++;
+        //     }
+        // }
+        // return cnt;
+
+        // better approach
+
+        int ans=0;
+        for(char c:s) ans=max(ans,(26-(c-'a'))%26);
+        return ans;
     }
 };
