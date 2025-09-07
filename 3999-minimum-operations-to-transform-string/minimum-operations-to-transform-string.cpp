6a6144c39cad34256c845e94b7ddb207d6b343c8
Purav Saluja <123455357+Purav001@users.noreply.github.com>
2025-09-07 11:41:18 +0530

    Time: 16 ms (63.16%) | Memory: 60.1 MB (0.00%) - LeetSync

---
+++ 3999-minimum-operations-to-transform-string/minimum-operations-to-transform-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minOperations(string s) {
+        vector<int> freq(27,0);
+        for(char c:s) freq[c-'a']++;
+        int cnt=0;
+        for(int i=1;i<26;i++){
+            if(freq[i]==0) continue;
+            else{
+                freq[i+1]+=freq[i];
+                freq[i]=0;
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
